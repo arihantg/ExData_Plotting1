df2890dfedffda151c91e5d72bcd6c49f582debf
arihantg <aribachhawat@gmail.com>
2014-05-10 20:47:44 +0530

    FORMATTING OF CODE

--- plot1.R
+++ plot1.R
@@ -1,8 +1,9 @@
 plot1<-function()
-{myfile <- "household_power_consumption.txt"
-mySql <- "SELECT * from file WHERE Date = '1/2/2007' OR Date = '2/2/2007'"
-myData <- read.csv.sql(myfile, sql=mySql, sep=";")
-png(file="plot1.png",bg = "transparent")
-hist(myData$Global_active_power,col="red",main="Global Active Power",xlab="Global Active Power(kilowatts)")
-dev.off()
+{
+	myfile <- "household_power_consumption.txt"
+	mySql <- "SELECT * from file WHERE Date = '1/2/2007' OR Date = '2/2/2007'"
+	myData <- read.csv.sql(myfile, sql=mySql, sep=";")
+	png(file="plot1.png",bg = "transparent")
+	hist(myData$Global_active_power,col="red",main="Global Active Power",xlab="Global Active Power(kilowatts)")
+	dev.off()
 }
--- plot3.R
+++ plot3.R
@@ -1,15 +1,14 @@
 plot3<-function()
-  {
-  myfile <- "household_power_consumption.txt"
-mySql <- "SELECT * from file WHERE Date = '1/2/2007' OR Date = '2/2/2007'"
-myData <- read.csv.sql(myfile, sql=mySql, sep=";")
-myData$dt<-paste(myData$Date,myData$Time," ")
-c<-strptime(myData$dt,format="%d/%m/%Y %H:%M:%S")
-png(file="plot3.png",bg = "transparent")
-plot(c,myData$Sub_metering_1,type="l",ylab="Energy Sub Metering")
-lines(c,myData$Sub_metering_2,col="red")
-lines(c,myData$Sub_metering_3,col="blue")
-box()
-legend('topright',c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c('black','red','blue'),lty=1,lwd=0)
-dev.off()
+{
+	myfile <- "household_power_consumption.txt"
+	mySql <- "SELECT * from file WHERE Date = '1/2/2007' OR Date = '2/2/2007'"
+	myData <- read.csv.sql(myfile, sql=mySql, sep=";")
+	myData$dt<-paste(myData$Date,myData$Time," ")
+	c<-strptime(myData$dt,format="%d/%m/%Y %H:%M:%S")
+	png(file="plot3.png",bg = "transparent")
+	plot(c,myData$Sub_metering_1,type="l",ylab="Energy Sub Metering")
+	lines(c,myData$Sub_metering_2,col="red")
+	lines(c,myData$Sub_metering_3,col="blue")
+	legend('topright',c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c('black','red','blue'),lty=1,lwd=0)
+	dev.off()
 }
